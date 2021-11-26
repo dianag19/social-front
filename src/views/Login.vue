489225db3b3d4d3042ae4c0111801f136702b0e2
Diana Gomez <diana.gomez.caicedo@correounivalle.edu.co>
2021-11-26 17:50:55 -0500

    login and register

---
+++ src/views/Login.vue
@@ -0,0 +1,52 @@
+<template>
+    <form @submit.prevent="submit">
+        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+        <div class="form-floating">
+          <input
+          v-model="data.email"
+            type="email"
+            class="form-control"
+            placeholder="name@example.com"
+            required
+          />
+        </div>
+        <div class="form-floating">
+          <input
+          v-model="data.password"
+            type="password"
+            class="form-control"
+            placeholder="Password"
+            required
+          />
+        </div>
+
+        <button class="w-100 btn btn-lg btn-primary" type="submit">
+          Sign in
+        </button>
+      </form>
+</template>
+<script>
+import { reactive } from 'vue';
+export default {
+    name:"Login",
+    setup(){
+      const data = reactive({
+        email:'',
+        password: ''
+      });
+
+      const submit = () => {
+
+      }
+      return{
+        data,
+        submit
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,56 @@
+<template>
+  <form @submit.prevent="submit">
+    <h1 class="h3 mb-3 fw-normal">Please register</h1>
+    <input
+    v-model="data.username"
+      class="form-control"
+      placeholder="Username"
+      required
+    />
+    <input
+    v-model="data.email"
+      type="email"
+      class="form-control"
+      placeholder="Email"
+      required
+    />
+    <input
+    v-model="data.password"
+      type="password"
+      class="form-control"
+      placeholder="Password"
+      required
+    />
+
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+  </form>
+</template>
+<script lang="ts">
+import { reactive} from 'vue';
+import { useRouter } from "vue-router";
+export default {
+  name: "Register",
+  setup() {
+      const data = reactive({
+          username: '',
+          email: '',
+          password: ''
+      });
+      const router = useRouter();
+
+        const submit = async () =>{
+            await fetch('http://localhost:8800/api/auth/register', {
+                method:"POST",
+                headers:{'Content-Type': 'application/json'},
+                body: JSON.stringify(data)
+            })
+            
+            await router.push('/login');
+        }
+      return{
+          data,
+          submit
+      }
+  }
+};
+</script>
